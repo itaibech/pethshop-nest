133cc004010bdbcc0b07d30a6a145e25883926d8
Itai Bechor <itai.bechor@gmail.com>
2023-06-13 09:26:36 +0300

    refactor mockDB Code and fix bugs #2

--- src/mongo.database/mongo.database.ts
+++ src/mongo.database/mongo.database.ts
@@ -53,16 +53,15 @@ export class MongoDatabase implements Database {
     return Promise.resolve(animals);
   }
 
-  getAnimalById(id: string): Promise<Animal | null> {
-    return new Promise((resolve, reject) => {
-      const findQuery = { _id: new ObjectId(id) };
-      this.collection.findOne(findQuery).then(animal => {
-        resolve(animal);
-      }).catch((err) => {
-        console.error(`Something went wrong trying to find the documents: ${err}\n`);
-        reject(err);
-      });
-    });
+  async getAnimalById(id: string): Promise<Animal | null> {
+    const findQuery = { _id: new ObjectId(id) };
+
+    try {
+      return await this.collection.findOne(findQuery);
+    } catch (err) {
+      console.error(`Something went wrong trying to find the documents: ${err}\n`);
+      throw err;
+    }
   }
 
   async updateAnimal(id: string, animal: Animal): Promise<boolean> {
@@ -105,7 +104,7 @@ export class MongoDatabase implements Database {
       }
     }
     try {
-      let animals = [];
+      let animals: any[];
       if (orderBy && direction && !Utils.isSimpleProperty(orderBy)) {
         animals = await this.findAndSortByAttributes(orderBy, direction, filter);
       } else {
@@ -144,7 +143,7 @@ export class MongoDatabase implements Database {
 
   private async findAndSortIfNeeded(orderBy: string, direction: string, filter: any) {
     if (orderBy && direction) {
-      let sortQuery: any = {};
+      let sortQuery: any;
       if (direction === "ASC") {
         sortQuery = { [orderBy]: 1 }; // Sort by field in ascending order
       } else {
